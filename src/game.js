5f4cc77c6d0af7e928f18e74626227a9a7bee886
Amber Bodnar <amber.bodnar@gmail.com>
2022-04-26 15:50:25 -0400

    Add win and draw to board display

--- src/game.js
+++ src/game.js
@@ -5,8 +5,9 @@ class Game {
     this.startingPlayer = this.player1;
     this.currentPlayer = this.player1;
     this.turnCounter = 0;
-    this.draw = false;
     this.gameCounter = 0;
+    this.win = false;
+    this.draw = false;
     this.board = [
       {id: "1", token: ""}, {id: "2", token: ""}, {id: "3", token: ""},
       {id: "4", token: ""}, {id: "5", token: ""}, {id: "6", token: ""},
@@ -22,28 +23,40 @@ class Game {
     }
   };
 
+  disableSquare(square) {
+    square.classList.add("disabled");
+  };
+
   switchPlayerTurn() {
     if (this.currentPlayer === this.player1) {
       this.currentPlayer = this.player2;
-      this.turnCounter ++;
     } else {
       this.currentPlayer = this.player1;
-      this.turnCounter ++;
     }
+
+    this.updateTurnCounter();
   };
 
-  disableSquare(square) {
-    square.classList.add("disabled");
+  updateTurnCounter() {
+    this.turnCounter ++;
+  };
+
+  checkForDraw() {
+    if (this.turnCounter === 8) {
+      return this.win = null;
+    }
   };
 
   // Horizontal wins by index position:
     // [0, 1, 2]
     // [3, 4, 5]
     // [6, 7, 8]
+
   checkForHorizontalWin() {
     if (this.board[0].token === this.currentPlayer.token && this.board[1].token === this.currentPlayer.token && this.board[2].token === this.currentPlayer.token ||
     this.board[3].token === this.currentPlayer.token && this.board[4].token === this.currentPlayer.token && this.board[5].token === this.currentPlayer.token ||
     this.board[6].token === this.currentPlayer.token && this.board[7].token === this.currentPlayer.token && this.board[8].token === this.currentPlayer.token) {
+      this.win = true;
       this.handleWin();
     }
   };
@@ -52,10 +65,12 @@ class Game {
     // [0, 3, 6]
     // [1, 4, 7]
     // [2, 5, 8]
+
   checkForVerticalWin() {
     if (this.board[0].token === this.currentPlayer.token && this.board[3].token === this.currentPlayer.token && this.board[6].token === this.currentPlayer.token ||
     this.board[1].token === this.currentPlayer.token && this.board[4].token === this.currentPlayer.token && this.board[7].token === this.currentPlayer.token ||
     this.board[2].token === this.currentPlayer.token && this.board[5].token === this.currentPlayer.token && this.board[8].token === this.currentPlayer.token) {
+      this.win = true;
       this.handleWin();
     }
   };
@@ -63,9 +78,11 @@ class Game {
   // Diagonal wins by index position:
     // [0, 4, 8]
     // [2, 4, 6]
+
   checkForDiagonalWin() {
     if (this.board[0].token === this.currentPlayer.token && this.board[0].token === this.currentPlayer.token && this.board[8].token === this.currentPlayer.token ||
     this.board[2].token === this.currentPlayer.token && this.board[4].token === this.currentPlayer.token && this.board[6].token === this.currentPlayer.token) {
+      this.win = true;
       this.handleWin();
     }
   };
@@ -73,9 +90,9 @@ class Game {
   handleWin() {
     this.currentPlayer.increaseWins();
     this.gameCounter ++;
-    // set the starting player
-    // switch the player turn display in the DOM
     // reset the gameBoard to ""
+    // switch the player turn display in the DOM
+    // set the starting player
   };
 
   determineStartingPlayer() {
@@ -100,7 +117,5 @@ class Game {
   //   ]
   // };
 
-  /*~~~~~~~still need: check for draw~~~~~*/
   /*~~~~~~~still need: reset board~~~~~~~~*/
-
 };
